bc7e318df77b3955cab8fdd17b5101891967cd29
dorut <dorut.iuga@inventiff.co>
2022-03-23 11:16:01 +0200

    removed set title

--- src/App.js
+++ src/App.js
@@ -1,54 +1,47 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import CardList from './components/card-list/card-list.component.jsx';
-import SearchBox from './components/search-box/search-box.component';
+import "./App.css";
+import { connect } from "redux";
+import { useState, useEffect } from "react";
+import CardList from "./components/card-list/card-list.component.jsx";
+import SearchBox from "./components/search-box/search-box.component";
 
 const App = () => {
-    const [searchField, setSearchField] = useState('');
-    const [monsters, setMonsters] = useState([]);
-    const [filteredMonsters, setFilteredMonsters] = useState(monsters);
-    const [title, setTitle] = useState('');
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then((response) => response.json())
-            .then((users) => {
-                setMonsters(users);
-            });
-    }, []);
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
-    useEffect(() => {
-        const newFilteredMonsters = monsters.filter((monster) => {
-            return monster.name.toLocaleLowerCase().includes(searchField);
-        });
-        setFilteredMonsters(newFilteredMonsters);
-    }, [monsters, searchField]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        setMonsters(users);
+      });
+  }, []);
 
-    const onSeachChange = (event) => {
-        const searchedFieldString = event.target.value.toLocaleLowerCase();
-        setSearchField(searchedFieldString);
-    };
-    const onTitleChange = (event) => {
-        const title = event.target.value.toLocaleLowerCase();
-        setTitle(title);
-    };
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
 
-    return (
-        <div className="App">
-            <h1 className="app-title">{title}</h1>
-            <SearchBox
-                onSeachHandler={onSeachChange}
-                hint="search monsters"
-                className="monsters-search-box"
-            />
-            <br />
-            <SearchBox
-                onSeachHandler={onTitleChange}
-                hint="set title"
-                className="monsters-search-box"
-            />
-            <CardList monsters={filteredMonsters} />
-        </div>
-    );
+  const onSeachChange = (event) => {
+    const searchedFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchedFieldString);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox
+        onSeachHandler={onSeachChange}
+        hint="search monsters"
+        className="monsters-search-box"
+      />
+      <br />
+
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
 };
 
 export default App;
