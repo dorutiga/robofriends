28a5e82b5a294e1945ea7391d323657ead848537
dorut <dorut.iuga@inventiff.co>
2022-03-23 11:21:41 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { connect } from "redux";
+
 import { useState, useEffect } from "react";
 import CardList from "./components/card-list/card-list.component.jsx";
 import SearchBox from "./components/search-box/search-box.component";
